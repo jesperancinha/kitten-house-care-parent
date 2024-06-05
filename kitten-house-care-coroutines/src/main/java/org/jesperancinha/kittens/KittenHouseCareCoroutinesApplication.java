f179d1aae15a2a4fcd85681cdde9e91e17425a16
Joao Esperancinha <jofisaes@gmail.com>
2024-06-05 21:18:26 +0200

    Baseline for Kotlin Koroutines

---
+++ kitten-house-care-coroutines/src/main/java/org/jesperancinha/kittens/KittenHouseCareCoroutinesApplication.java
@@ -0,0 +1,13 @@
+package org.jesperancinha.kittens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittenHouseCareCoroutinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittenHouseCareCoroutinesApplication.class, args);
+	}
+
+}
---
+++ kitten-house-care-coroutines/src/test/java/org/jesperancinha/kittens/KittenHouseCareCoroutinesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jesperancinha.kittens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittenHouseCareCoroutinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
